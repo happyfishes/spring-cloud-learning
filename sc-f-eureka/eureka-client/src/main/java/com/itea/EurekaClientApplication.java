6017024e883bec775ad82f79c502cfafbfa37972
happyfishes <ghbstart@163.com>
2019-03-27 20:42:39 +0800

    sc-f-eureka

---
+++ sc-f-eureka/eureka-client/src/main/java/com/itea/EurekaClientApplication.java
@@ -0,0 +1,29 @@
+package com.itea;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableEurekaClient
+@SpringBootApplication
+@RestController
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi/{msg}")
+    public String hi(@PathVariable("msg") String msg){
+        return "I am " + msg + ", My port " + port;
+    }
+
+
+}
---
+++ sc-f-eureka/eureka-client/src/test/java/com/itea/EurekaClientApplicationTests.java
@@ -0,0 +1,14 @@
+package com.itea;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+public class EurekaClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
