c772d0946dd8f2c2a46e273b48b36bb4b4d50cf6
happyfishes <ghbstart@163.com>
2019-03-29 23:13:28 +0800

    hystrix

---
+++ sc-f-hystrix/service-hi/src/main/java/com/itea/ServiceHiApplication.java
@@ -0,0 +1,29 @@
+package com.itea;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableEurekaClient
+@SpringBootApplication
+@RestController
+public class ServiceHiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHiApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String hi(@RequestParam("msg") String msg){
+        return "I am " + msg + ", My port " + port;
+    }
+
+}
